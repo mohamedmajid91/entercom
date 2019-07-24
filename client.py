e6c99cba5ce2df6443d67abd2a62cba4dfc0d5a5
Emirhan Pehlevan <53257417+emirhanpeh@users.noreply.github.com>
2019-07-24 15:27:18 +0300

    Update client.py

--- client.py
+++ client.py
@@ -43,17 +43,7 @@ while True:
         x1=table[1]
         x1f=int(x1)
 
-
-
-
-        
         print(tabulate([[loss,table[0],table[1],table[2],table[3],table[4]]], headers=['LOSS PCKTS','TOTAL PCKTS','CHANNEL_1',"CHANNEL_2","CHANNEL_3","CHANNEL_4","CHANNEL_5"]))
-
-
-
-
-
-
 
         #print(x1f)
         x1fx=interp(x1f,[0,1024],[0,180])
@@ -61,37 +51,6 @@ while True:
 
     else:
         loss=loss+1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 """
     if x1f < 0.5:
         board.digital[dir1].write(0)
@@ -113,23 +72,3 @@ while True:
         print(x1fx)
         board.digital[pin].write(x1fx)
 """      
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
